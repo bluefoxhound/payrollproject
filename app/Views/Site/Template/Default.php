4ba3573d0996e504cbcef20b79d84e584dec397f
Raf Cat <fox_3rc@yahoo.com>
2022-06-07 19:13:38 +0800

    Update 3 07062022

---
+++ app/Views/Site/Template/Default.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payroll Project</title>
+    <link rel="stylesheet" href="<?php echo base_url('resource/css/main.css?'.time()); ?>">
+    <link rel="icon" href="<?php echo base_url('favicon.ico?'.time()); ?>" />
+</head>
+<body>
+    <?php $this->renderSection('content') ?>
+</body>
+</html>
